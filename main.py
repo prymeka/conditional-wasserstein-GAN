6a2aff867030e6ce1b99460a41721358270c7d10
Aleksander Prymek <prymeka@tcd.ie>
2023-03-06 17:50:58 +0000

    created cli for training wgan

--- main.py
+++ main.py
@@ -2,63 +2,26 @@ import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from keras.datasets import fashion_mnist
 import numpy as np
+import sys
 
-from typing import Tuple
+from typing import Tuple, Union
 
-from discriminator import get_discriminator_model
-from generator import get_generator_model
-from wgan import ConditionalWGAN
-from callbacks import GANMonitor
-from utils import discriminator_loss, generator_loss
+from utils import fit_wgan, load_dataset
 
 
-def run(
-    train_images: np.ndarray,
-    train_labels: np.ndarray,
-    epochs: int,
-    batch_size: int,
-    noise_dim: int = 128,
-    input_shape: Tuple[int, int, int] = (28, 28, 3),
-) -> None:
-
-    generator_optimizer = Adam(
-        learning_rate=0.0002, beta_1=0.5, beta_2=0.9
+def main(dataset: str, epochs: Union[int, str], batch_size: Union[int, str] = 128, noise_dim: Union[int, str] = 128) -> None:
+    (x_train, y_train), (x_test, y_test), key = load_dataset(dataset)
+    y_train = y_train.reshape((-1, 1))
+    input_shape = (28, 28, 3) if dataset != 'cifar' else (32, 32, 3)
+    fit_wgan(
+        train_images=x_train,
+        train_labels=y_train,
+        epochs=int(epochs),
+        batch_size=int(batch_size),
+        noise_dim=int(noise_dim),
+        input_shape=input_shape,
     )
-    discriminator_optimizer = Adam(
-        learning_rate=0.0002, beta_1=0.5, beta_2=0.9
-    )
-
-    cbk = GANMonitor(num_imgs=3, latent_dim=noise_dim)
-    wgan = ConditionalWGAN(
-        image_shape=input_shape,
-        discriminator=get_discriminator_model(input_shape),
-        generator=get_generator_model(noise_dim),
-        latent_dim=noise_dim,
-        discriminator_extra_steps=3,
-    )
-    wgan.compile(
-        d_optimizer=discriminator_optimizer,
-        g_optimizer=generator_optimizer,
-        g_loss_fn=generator_loss,
-        d_loss_fn=discriminator_loss,
-    )
-    wgan.fit(train_images, train_labels, batch_size=batch_size,
-             epochs=epochs, callbacks=[cbk], verbose=1)
-
-    wgan.generator.save(f'models/g_{epochs}epochs')
-    wgan.generator.save_weights(f'models/g_weights_{epochs}epochs')
-    wgan.discriminator.save(f'models/d_{epochs}epochs')
-    wgan.discriminator.save_weights(f'models/d_weights_{epochs}epochs')
 
 
 if __name__ == '__main__':
-    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-    y_train = y_train.reshape((-1, 1))
-    run(
-        epochs=50,
-        input_shape=(28, 28, 1),
-        noise_dim=128,
-        batch_size=512,
-        train_images=x_train,
-        train_labels=y_train
-    )
+    main(sys.argv[1:])
--- utils.py
+++ utils.py
@@ -2,7 +2,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 
-from typing import Literal
+from discriminator import get_discriminator_model
+from generator import get_generator_model
+from wgan import ConditionalWGAN
+from callbacks import GANMonitor
+
+from typing import Dict, Literal, Tuple
 
 def discriminator_loss(real_img: tf.Tensor, fake_img: tf.Tensor) -> tf.Tensor:
     real_loss = tf.reduce_mean(real_img)
@@ -12,7 +17,7 @@ def discriminator_loss(real_img: tf.Tensor, fake_img: tf.Tensor) -> tf.Tensor:
 def generator_loss(fake_img: tf.Tensor) -> tf.Tensor:
     return -tf.reduce_mean(fake_img)
 
-def load_dataset(name: Literal['mnist', 'fmnist', 'cifar']) -> tuple[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray], dict[int, str]]:
+def load_dataset(name: Literal['mnist', 'fmnist', 'cifar']) -> Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray], Dict[int, str]]:
     (x_train, y_train), (x_test, y_test) = {
         'mnist': tf.keras.datasets.mnist.load_data,
         'fmnist': tf.keras.datasets.fashion_mnist.load_data,
@@ -67,3 +72,42 @@ def load_dataset(name: Literal['mnist', 'fmnist', 'cifar']) -> tuple[tuple[np.nd
     }[name]
     
     return (x_train, y_train), (x_test, y_test), key
+
+
+def fit_wgan(
+    train_images: np.ndarray,
+    train_labels: np.ndarray,
+    epochs: int,
+    batch_size: int,
+    noise_dim: int = 128,
+    input_shape: Tuple[int, int, int] = (28, 28, 3),
+) -> None:
+
+    generator_optimizer = Adam(
+        learning_rate=0.0002, beta_1=0.5, beta_2=0.9
+    )
+    discriminator_optimizer = Adam(
+        learning_rate=0.0002, beta_1=0.5, beta_2=0.9
+    )
+
+    cbk = GANMonitor(num_imgs=3, latent_dim=noise_dim)
+    wgan = ConditionalWGAN(
+        image_shape=input_shape,
+        discriminator=get_discriminator_model(input_shape),
+        generator=get_generator_model(noise_dim),
+        latent_dim=noise_dim,
+        discriminator_extra_steps=3,
+    )
+    wgan.compile(
+        d_optimizer=discriminator_optimizer,
+        g_optimizer=generator_optimizer,
+        g_loss_fn=generator_loss,
+        d_loss_fn=discriminator_loss,
+    )
+    wgan.fit(train_images, train_labels, batch_size=batch_size,
+             epochs=epochs, callbacks=[cbk], verbose=1)
+
+    wgan.generator.save(f'models/g_{epochs}epochs')
+    wgan.generator.save_weights(f'models/g_weights_{epochs}epochs')
+    wgan.discriminator.save(f'models/d_{epochs}epochs')
+    wgan.discriminator.save_weights(f'models/d_weights_{epochs}epochs')
